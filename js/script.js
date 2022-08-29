87980ab74affc9c5473c637a444721c434fa03a1
pepepepepei <jumpei.yamada.nagoya@gmail.com>
2022-08-29 14:07:47 +0900

    直前のコミットのカッコの位置を変更

--- js/script.js
+++ js/script.js
@@ -148,7 +148,7 @@ function insertSeekBarValue(seconds) {
 
 function formatSeconds(seconds) {
   const minutes = Math.floor(seconds / 60);
-  const remainderSeconds = Math.floor((seconds % 60).toString().padStart(2, '0'));
+  const remainderSeconds = Math.floor(seconds % 60).toString().padStart(2, '0');
   return minutes + ':' + remainderSeconds;
 }
 
